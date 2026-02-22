2482d31b3b69136fda5a2d9fe812c347d0460699
Richardnag <rnarroyog24@ul.edu.co>
2026-02-22 14:06:43 -0500

    PRIMER Y SEGUNDO APELLIDO DART
    Primer y segundo apellido funciona correctamente

--- REGISTRO-DART.dart
+++ REGISTRO-DART.dart
@@ -8,4 +8,10 @@ void main() {
 
   stdout.write("Segundo Nombre: ");
   String nom2 = stdin.readLineSync() ?? '';
+
+  stdout.write("Primer Aprellido: ");
+  String ape1 = stdin.readLineSync() ?? '';
+
+  stdout.write("Segundo Nombre: ");
+  String ape2 = stdin.readLineSync() ?? '';
 }
